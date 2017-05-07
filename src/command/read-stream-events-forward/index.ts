33f1b60833972c04e0d395681136e80895351875
Chris McLeod <chris12295@gmail.com>
2017-05-07 12:16:59 -0500

    Add read stream events forward command

---
+++ src/command/read-stream-events-forward/index.ts
@@ -0,0 +1,8 @@
+import { Command as C } from "../command";
+import { eventstore } from "esproto";
+
+export const CODE = 0xB2;
+export type CODE = typeof CODE;
+export interface Command extends C<CODE, eventstore.proto.ReadStreamEvents> { }
+export type Params = eventstore.proto.ReadStreamEvents$Properties;
+export const Message = eventstore.proto.ReadStreamEvents;
